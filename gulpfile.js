1eb1afb9f7b7032cf6867ea565e9eb7d66416ba8
stefan judis <stefanjudis@gmail.com>
2017-10-09 23:54:52 +0200

    Bring bootstrap into sass

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ var banner = ['/*!\n',
 
 gulp.task('sass', function() {
   return gulp.src('scss/agency.scss')
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(header(banner, {
       pkg: pkg
     }))
@@ -77,23 +77,6 @@ gulp.task('images', function() {
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('dist/vendor/bootstrap'))
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('dist/vendor/jquery'))
-
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('dist/vendor/popper'))
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('dist/vendor/jquery-easing'))
-
   gulp.src([
       'node_modules/font-awesome/**',
       '!node_modules/font-awesome/**/*.map',
